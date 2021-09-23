d912d2dd09357275a5180bb7e12eb46df76c01b8
SnigdhaP21 <86755653+SnigdhaP21@users.noreply.github.com>
2021-09-22 20:07:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function spfuna() {
+    window.alert("All our dreams can come true, if we have the courage to pursue them. - Walt Disney");
+}
+function spfunb() {
+    window.alert("The secret of getting ahead, is getting started. - Mark Twain");
+}
+
+{document.getElementById("audio").play();
+}
